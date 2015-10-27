6166585480cfd8c050a028f7bd6790c344a1f53d
Roberto <roberto@robertocalderon.ca>
2015-10-27 11:48:10 -0700

    working prototype for out node

--- sox/sox.js
+++ sox/sox.js
@@ -67,6 +67,7 @@ module.exports = function(RED) {
         };
         soxEventListener.sensorDataReceived = function(soxEvent){
           if (soxEvent.transducers && soxEvent.transducers.length > 0) {
+            //TODO: For each publish one message?
             node.send( {payload: soxEvent.transducers});
           }
         };
@@ -93,9 +94,9 @@ module.exports = function(RED) {
          RED.nodes.createNode(this,n);
          var node = this;
 
-         if (!n.device) {
-             node.error("No device specified");
-             return;
+         if (!n.device){
+           node.error("No device specified");
+           return;
          }
 
          this.login = RED.nodes.getNode(n.login);// Retrieve the config node
@@ -103,18 +104,96 @@ module.exports = function(RED) {
              node.error("No credentials specified");
              return;
          }
-         /*
-         this.sensor = n.sensor;
-         //this.bosh = this.login.url || "url.com";
-         //etc...
-         this.on("input",function(msg) {
-             if (msg.payload !== undefined) {
-                 // Accepted formats: Formated Object.
-                 //sendevent
-             }
-         });
-         */
+
+         this.device = n.device;
+         //this.url = this.login.url || "http://wotkit.sensetecnic.com";
+
+         this.bosh = this.login.bosh || "http://sox.ht.sfc.keio.ac.jp:5280/http-bind/";
+         this.xmpp = this.login.xmpp || "sox.ht.sfc.keio.ac.jp";
+         this.jid = this.login.jid;// || "sensorizer@sox.ht.sfc.keio.ac.jp";
+         this.password = this.login.password;// || "miromiro";
+         //var sensorName = "hcttest";
+
+         if (this.bosh && this.xmpp && this.jid && this.password && this.device) {
+           var deviceName = this.device;
+
+            var client = new SoxClient.SoxClient(this.bosh, this.xmpp, this.jid, this.password);
+          	//var client = new SoxClient(boshService, xmppServer);
+
+          	var soxEventListener = new SoxEventListener.SoxEventListener();
+          	soxEventListener.connected = function(soxEvent) {
+          		/**
+          		 * we are successfully connected to the server
+          		 */
+          		node.warn("[main.js] Connected "+soxEvent.soxClient);
+          		//status("Connected: "+soxEvent.soxClient);
+
+          		/**
+          		 * CREATE DEVICE INSTANCE
+          		 * first create a device specifying just a name
+          		 */
+          		var device = new Device.Device(deviceName);//デバイス名に_dataや_metaはつけない
+
+          		/**
+          		 * try to get the device's internal information from the server
+          		 */
+          		if(!client.resolveDevice(device)){
+          			/* we are failed. manually construct the device  */
+          			node.warn("Warning: Couldn't resolve device: "+device+". Continuing...");
+          			var transducer = new Transducer.Transducer();//create a transducer
+          			transducer.name = "occupancy"; //TODO: from node conf
+          			transducer.id = "occupancy"; //TODO: from node conf
+          			device.addTransducer(transducer);//add the transducer to the device
+          			var data = new SensorData.SensorData("occupancy", new Date(), "1", "2");//create a value to publish //TODO: grab value from input
+          			transducer.setSensorData(data);//set the value to the transducer
+          			soxEvent.soxClient.publishDevice(device);//publish
+          		}
+          	};
+          	soxEventListener.connectionFailed = function(soxEvent) {
+          		node.warn("Connection Failed: "+soxEvent.soxClient);
+          	};
+          	soxEventListener.resolved = function(soxEvent){
+          		/**
+          		 * successfully acquired the device's internal information from the server
+          		 */
+          		node.warn("Resolved: "+soxEvent.device);
+
+          		/**
+          		 * specify the transducer to publish
+          		 */
+          		var transducer = soxEvent.device.getTransducer("occupancy"); //TODO: from node conf
+
+          		/**
+          		 * create a value
+          		 */
+          		var data = new SensorData.SensorData("occupancy", new Date(), "空車", "空車"); //TODO: ARGH more ?
+
+          		/**
+          		 * set the value to the transducer
+          		 */
+          		transducer.setSensorData(data);
+
+          		/**
+          		 * publish
+          		 */
+          		soxEvent.soxClient.publishDevice(soxEvent.device);
+          	};
+          	soxEventListener.resolveFailed = function(soxEvent){
+          		node.warn("Resolve Failed: "+soxEvent.device);
+          	};
+          	soxEventListener.published = function(soxEvent){
+          		node.warn("Published: "+soxEvent.device);
+          	};
+          	soxEventListener.publishFailed = function(soxEvent){
+          		node.warn("Publish Failed: "+soxEvent.device+" errorCode="+soxEvent.errorCode+" errorType="+soxEvent.errorType);
+          	};
+
+          	client.setSoxEventListener(soxEventListener);
+          	client.connect();
+         }
+
          this.on('close', function(){
+             client.disconnect();
              node.status({});
          });
 

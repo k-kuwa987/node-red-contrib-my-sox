4778e061f4ef7a2feeb608ffa25af0b8ddc9ca95
Nam Giang <kyng@ece.ubc.ca>
2016-03-20 02:50:29 -0700

    new test

---
+++ test/testNodeStrophe.js
@@ -0,0 +1,6 @@
+var jsdom = require('jsdom').jsdom;
+var document = jsdom('<html></html>', {});
+var window = document.defaultView;
+var $ = require('../../jquery')(window);
+
+var f = $("<fieldset>");

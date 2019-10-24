1b160d45b88758059b09a2d9ee61d36c3812c518
Kazutoshi  Uetsuhara <kazutoshi.u@gmail.com>
2019-10-24 11:48:07 +0900

    discoverのsendを追加

--- sox/discover.js
+++ sox/discover.js
@@ -7,7 +7,7 @@ module.exports = function(RED) {
   function SoxDiscoverNode(config) {
     RED.nodes.createNode(this, config)
 
-    this.login = RED.nodes.getNode(config.login) // Retrieve the config node
+    this.login = RED.nodes.getNode(config.login)
     if (!this.login) {
       node.error('No credentials specified')
       return
@@ -21,18 +21,17 @@ module.exports = function(RED) {
     var node = this
 
     node.on('input', function() {
-      console.log('hooo this is discover!')
-
-      this.client = new SoxConnection(this.bosh, this.xmpp)
+      node.client = new SoxConnection(this.bosh, this.xmpp)
 
       node.client.connect(() => {
         console.log('sox connected')
-        this.client.fetchDevices(function(devices) {
-          console.log(devices)
+        node.client.fetchDevices(function(devices) {
+          var devicesArray = []
           for (var i = 0; i < devices.length; i++) {
-            var device = devices[i]
-            console.log(device.getName())
+            devicesArray.push(devices[i].getName())
           }
+          console.log(devicesArray)
+          node.send({ payload: devicesArray })
           // 接続終了
           node.client.disconnect()
           console.log('@@@ disconnected')

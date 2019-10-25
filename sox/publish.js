d83c55eee3c0045ca5e65fbf952d312dfb05f95c
Kazutoshi  Uetsuhara <kazutoshi.u@gmail.com>
2019-10-25 15:50:26 +0900

    publish node operating checked

--- sox/publish.js
+++ sox/publish.js
@@ -2,6 +2,8 @@
 var DEFAULT_BOSH = 'http://sox.ht.sfc.keio.ac.jp:5280/http-bind/'
 var DEFAULT_XMPP = 'sox.ht.sfc.keio.ac.jp'
 var SoxConnection = require('soxjs2').SoxConnection
+var TransducerValue = require('soxjs2').TransducerValue
+var Data = require('soxjs2').Data
 
 module.exports = function(RED) {
   'use strict'
@@ -23,8 +25,42 @@ module.exports = function(RED) {
 
     var node = this
 
-    node.on('input', function() {
-      // fixme
+    node.on('input', function(msg) {
+      node.client = new SoxConnection(node.bosh, node.xmpp)
+      var domain = node.client.getDomain()
+
+      node.client.connect(() => {
+        node.status({ fill: 'green', shape: 'dot', text: 'request...' })
+
+        var device = node.client.bind(node.device, domain)
+        var values = []
+        Object.keys(msg.transducer).forEach(function(key) {
+          console.log('key = ' + key)
+          console.log('value = ' + msg.transducer[key])
+          var t_val = new TransducerValue(
+            key,
+            msg.transducer[key],
+            msg.transducer[key]
+          )
+          values.push(t_val)
+        })
+
+        var data = new Data(device, values)
+
+        var suc = function() {
+          console.log('\n\n@@@@ suc\n\n')
+          node.send({ payload: 'Success' })
+          node.status({})
+        }
+
+        var err = function() {
+          console.log('\n\n@@@@ err\n\n')
+          node.send({ payload: 'Error' })
+          node.status({ fill: 'red', shape: 'dot', text: 'error' })
+        }
+
+        node.client.publish(data, suc, err)
+      })
     })
 
     node.on('close', function() {
